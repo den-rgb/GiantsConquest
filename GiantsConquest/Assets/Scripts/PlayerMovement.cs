146b0201835f277ee933a6169ca30309a6bf2afc
den-rgb <deni.m2709@gmail.com>
2022-12-30 17:04:46 +0000

    Added better movement + cinemachine

--- GiantsConquest/Assets/Scripts/PlayerMovement.cs
+++ GiantsConquest/Assets/Scripts/PlayerMovement.cs
@@ -4,14 +4,27 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 5.0f; // movement speed of the character
+    // movement speed of the character
+    public float speed = 5.0f;
 
-    public bool isOnGround = false; // variable to track if the character is on the ground
 
-    public CapsuleCollider LfootColider; // the left foot collider to check for ground contact
-    public CapsuleCollider RfootColider; // the right foot collider to check for ground contact
+    // variable to track if the character is on the ground
+    public bool isOnGround = false;
 
-    public float rotationSpeed = 90.0f; // the speed at which the character rotates towards the movement direction
+    // variable to track if the characters feet are on the ground
+    public bool pushOff = false;
+
+    // the left foot collider to check for ground contact
+    public CapsuleCollider LfootColider;
+
+    // the right foot collider to check for ground contact
+    public CapsuleCollider RfootColider;
+
+    // the sphere collider to check for ground contact
+    public SphereCollider basicCollider;
+
+    // the speed at which the character rotates towards the movement direction
+    public float rotationSpeed = 90.0f;
 
     // Update is called once per frame
     void Update()
@@ -29,8 +42,50 @@ public class PlayerMovement : MonoBehaviour
         movementDirection = movementDirection.normalized;
 
         // Check if either the left foot collider or the right foot collider is touching the ground
-        isOnGround = Physics.CheckCapsule(LfootColider.bounds.center, RfootColider.bounds.center, LfootColider.radius, LayerMask.GetMask("Ground")) ||
+        pushOff = Physics.CheckCapsule(LfootColider.bounds.center, RfootColider.bounds.center, LfootColider.radius, LayerMask.GetMask("Ground")) ||
                      Physics.CheckCapsule(LfootColider.bounds.center, RfootColider.bounds.center, RfootColider.radius, LayerMask.GetMask("Ground"));
+
+        // Check if the sphere collider is touching the ground
+        isOnGround = Physics.CheckSphere(basicCollider.bounds.center, basicCollider.radius, LayerMask.GetMask("Ground"));
+
+        // if the forward key is pressed and the character is on the ground, use the current forward direction of the character as the movement direction
+        if (verticalInput > 0 && horizontalInput > 0)
+        {
+            movementDirection = (transform.forward + transform.right).normalized;
+        }
+        else if (verticalInput > 0 && horizontalInput < 0)
+        {
+            movementDirection = (transform.forward - transform.right).normalized;
+        }
+        else if (verticalInput < 0 && horizontalInput > 0)
+        {
+            movementDirection = (-transform.forward + transform.right).normalized;
+        }
+        else if (verticalInput < 0 && horizontalInput < 0)
+        {
+            movementDirection = (-transform.forward - transform.right).normalized;
+        }
+        else if (verticalInput > 0)
+        {
+            movementDirection = transform.forward;
+        }
+        else if (verticalInput < 0)
+        {
+            movementDirection = -transform.forward;
+        }
+        else if (horizontalInput > 0)
+        {
+            movementDirection = transform.right;
+        }
+        else if (horizontalInput < 0)
+        {
+            movementDirection = -transform.right;
+        }
+        else
+        {
+            movementDirection = transform.forward;
+        }
+
 
         // Rotate the character's body towards the movement direction gradually
         transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(movementDirection), rotationSpeed * Time.deltaTime);
@@ -38,8 +93,17 @@ public class PlayerMovement : MonoBehaviour
         // Only move the character if it is on the ground and receiving input
         if (isOnGround && (horizontalInput != 0 || verticalInput != 0))
         {
+            if (pushOff)
+            {
+                speed = 15f;
+            }
+            else
+            {
+                speed = 10f;
+            }
             // Move the character in the movement direction with the set speed
             transform.position += movementDirection * speed * Time.deltaTime;
         }
+
     }
 }
